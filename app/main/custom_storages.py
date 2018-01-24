32c587a1af765cb336dd0b237e6f22815237f2d7
Marcus Weiner <marcus.weiner@gmail.com>
2018-01-24 17:19:35 +0100

    :ambulance: Supply missing custom storage for random names

---
+++ app/main/custom_storages.py
@@ -0,0 +1,11 @@
+import os
+import uuid
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class S3BotoRandomNameStorage(S3Boto3Storage):
+    def generate_filename(self, filename):
+        filename = super().generate_filename(filename)
+        extension = os.path.splitext(filename)[1]
+        path = os.path.split(filename)[0]
+        return os.path.join(path, "%s%s") % (uuid.uuid4(), extension)
