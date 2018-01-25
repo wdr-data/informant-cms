e945ccddf5ab759e1b3c990dc2e803d2c059e917
ChristianJoe <cjoe@gmx.de>
2018-01-25 18:35:38 +0100

    :sparkles: Allow querying of report with fragments

--- app/api/views/reports.py
+++ app/api/views/reports.py
@@ -1,7 +1,10 @@
-from cms.models.report import Report
+from distutils.util import strtobool
+
+from cms.models.report import Report, ReportFragment
 from .tags import TagSerializer
 from .genres import GenreSerializer
 from .topics import TopicSerializer
+from .fragments import ReportFragmentSerializer
 
 from rest_framework import serializers, viewsets
 
@@ -16,6 +19,28 @@ class ReportSerializer(serializers.ModelSerializer):
         fields = ('id', 'created', 'genres', 'topic', 'tags', 'headline', 'text', 'media',
                   'media_original', 'media_note', 'published', 'delivered')
 
+    def __init__(self, *args, with_fragments=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.with_fragments = with_fragments
+
+    def to_representation(self, obj):
+        rep = super().to_representation(obj)
+
+        if not self.with_fragments:
+            return rep
+
+        serializer = ReportFragmentSerializer(many=True, read_only=True)
+        fragments = ReportFragment.objects.filter(report=obj)
+        next_fragments = []
+
+        for fragment in fragments:
+            next_fragments.append(fragment)
+            if fragment.question:
+                break
+
+        rep['fragments'] = serializer.to_representation(next_fragments)
+        return rep
+
 
 class ReportViewSet(viewsets.ModelViewSet):
     """
@@ -24,3 +49,9 @@ class ReportViewSet(viewsets.ModelViewSet):
     queryset = Report.objects.all().order_by('-created')
     serializer_class = ReportSerializer
     filter_fields = ('genres', 'topic', 'tags')
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['with_fragments'] = bool(
+            strtobool(self.request.query_params.get('withFragments', 'no'))
+        )
+        return super().get_serializer(*args, **kwargs)
