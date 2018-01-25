b41fee7c6edbecc56bfe3f3ce413a72b95bfb028
Marcus Weiner <marcus.weiner@gmail.com>
2018-01-25 19:23:17 +0100

    :recycle: Move next fragment behaviour into base class

--- app/api/views/fragments.py
+++ app/api/views/fragments.py
@@ -6,31 +6,30 @@ from distutils.util import strtobool
 from rest_framework import serializers, viewsets
 
 
+class BaseFragmentViewSet(viewsets.ModelViewSet):
+    def get_serializer(self, *args, **kwargs):
+        serializer_class = self.get_serializer_class()
+        with_next = strtobool(self.request.query_params.get('withNext', "0"))
+
+        class WithNextSerializer(serializer_class):
+            def to_representation(self, obj):
+                output = super().to_representation(obj)
+                if with_next:
+                    output['next_fragments'] = augment_fragments(obj.report, obj.id)
+                return output
+
+        kwargs['context'] = self.get_serializer_context()
+        return WithNextSerializer(*args, **kwargs)
+
+
 class ReportFragmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReportFragment
         fields = ('id', 'question', 'text', 'media_original', 'media', 'media_note',
-            'link_wiki', 'report')
-
-    def __init__(self, *args, with_next=False, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._with_next = with_next
-
-    def to_representation(self, obj):
-        output = super().to_representation(obj)
-        if self._with_next:
-            output['next_fragments'] = augment_fragments(obj.report, obj.id)
-        return output
+                  'link_wiki', 'report')
 
 
-class ReportFragmentViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
+class ReportFragmentViewSet(BaseFragmentViewSet):
     queryset = ReportFragment.objects.all().order_by('id')
     serializer_class = ReportFragmentSerializer
-    filter_fields = ('report', )
-
-    def get_serializer(self, *args, **kwargs):
-        kwargs['with_next'] = strtobool(self.request.query_params.get('withNext', "0"))
-        return super().get_serializer(*args, **kwargs)
+    filter_fields = ('report',)
