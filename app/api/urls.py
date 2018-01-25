09ac5b7c3a7a8bc34f8daf86db45c60d06fcbf3c
Jannes Höke <jhoeke@uni-bremen.de>
2018-01-25 10:31:22 +0100

    :sparkles: Add faqs endpoint to REST api

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,8 @@
-from .views import reports, genres, topics, tags
+from .views import reports, genres, topics, tags, faqs
 
 urlpatterns = []
 urlpatterns += reports.urlpatterns
 urlpatterns += genres.urlpatterns
 urlpatterns += topics.urlpatterns
 urlpatterns += tags.urlpatterns
+urlpatterns += faqs.urlpatterns
---
+++ app/api/views/faqs.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers, viewsets, routers
+from django.conf.urls import url, include
+
+from cms.models.faq import FAQ
+
+
+class FAQSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FAQ
+        fields = ('id', 'name', 'slug', 'text', 'media', 'media_original', 'media_note')
+
+
+class FAQViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = FAQ.objects.all().order_by('id')
+    serializer_class = FAQSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'faqs', FAQViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
