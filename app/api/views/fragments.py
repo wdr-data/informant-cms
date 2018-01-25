a7db3a98ccb57089de23740e7e289df78311b8bb
LizzAy <lisachenbach@gmx.de>
2018-01-25 17:58:53 +0100

    :sparkles: Add next fragment field to fragment api

--- app/api/views/fragments.py
+++ app/api/views/fragments.py
@@ -9,9 +9,17 @@ class ReportFragmentSerializer(serializers.ModelSerializer):
         fields = ('id', 'question', 'text', 'media_original', 'media', 'media_note',
             'link_wiki', 'report')
 
+    def to_representation(self, obj):
+        output = super().to_representation(obj)
+        fragments = ReportFragment.objects.filter(report=obj.report, id__gt=obj.id)
+        fragment_out = [super(ReportFragmentSerializer, self).to_representation(fragment) for fragment in fragments]
+        output['next_fragments'] = fragment_out
+        return output
+
 class ReportFragmentViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = ReportFragment.objects.all().order_by('id')
     serializer_class = ReportFragmentSerializer
+    filter_fields = ('report', )
