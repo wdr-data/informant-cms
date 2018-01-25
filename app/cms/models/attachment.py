e1d4b0ebbf30e87706d87e98eeb1fbd680ac6b76
Jannes Höke <jhoeke@uni-bremen.de>
2018-01-25 20:21:18 +0100

    :sparkles: Resize images if their size exceeds 1920x1080

--- app/cms/models/attachment.py
+++ app/cms/models/attachment.py
@@ -31,10 +31,6 @@ class Attachment(models.Model):
             self.media = None
             return
 
-        if not self.media_note:
-            self.media = self.media_original
-            return
-
         try:
             file_content = requests.get(self.media_original.url).content
         except MissingSchema:
@@ -47,47 +43,65 @@ class Attachment(models.Model):
             self.media = self.media_original
             raise
 
-        # Create initial image objects
+        image_changed = False
         orig_mode = img.mode
-        img = img.convert('RGBA')
-        alpha = Image.new('RGBA', img.size, (0, 0, 0, 0))
-        draw = ImageDraw.Draw(alpha)
+        max_size = (1920, 1080)
 
-        # Configuration
-        the_text = 'FOTO: ' + self.media_note.upper()
-        fontsize = max((img.size[0] + img.size[1]) / 2 / 50, 10)
-        shadow_radius = fontsize / 3
-        shadow_mult = 0.75
-        text_alpha = 0.7
-        padding = tuple((int(shadow_radius * 2.5), int(shadow_radius * 3)))
-        position = tuple(int(shadow_radius + p) for p in padding)
+        # Resize image
+        if any(actual > max_ for actual, max_ in zip(img.size, max_size)):
+            scale = min(max_ / actual for max_, actual in zip(max_size, img.size))
 
-        fnt = ImageFont.truetype(
-            str(ASSETS_DIR / 'Open_Sans' / 'OpenSans-SemiBold.ttf'), int(fontsize))
+            new_size = tuple(int(dim * scale) for dim in img.size)
+            img = img.resize(new_size, resample=Image.LANCZOS)
+            image_changed = True
 
-        # Calculate size of text
-        text_size = draw.textsize(the_text, font=fnt)
+        # Draw credit onto image
+        if self.media_note:
+            # Create initial image objects for text drawing
+            img = img.convert('RGBA')
+            alpha = Image.new('RGBA', img.size, (0, 0, 0, 0))
+            draw = ImageDraw.Draw(alpha)
 
-        # Create new, smaller image to draw the text on
-        txt = Image.new(
-            'RGBA',
-            tuple(ts + pad + pos for ts, pad, pos in zip(text_size, padding, position)),
-            (0, 0, 0, 0))
+            # Configuration for text drawing
+            the_text = 'FOTO: ' + self.media_note.upper()
+            fontsize = max((img.size[0] + img.size[1]) / 2 / 50, 10)
+            shadow_radius = fontsize / 3
+            shadow_mult = 0.75
+            text_alpha = 0.7
+            padding = tuple((int(shadow_radius * 2.5), int(shadow_radius * 3)))
+            position = tuple(int(shadow_radius + p) for p in padding)
 
-        # Do the drawing
-        draw = ImageDraw.Draw(txt)
-        draw.text(position, the_text, font=fnt, fill=(0, 0, 0, 255))
+            fnt = ImageFont.truetype(
+                str(ASSETS_DIR / 'Open_Sans' / 'OpenSans-SemiBold.ttf'), int(fontsize))
 
-        shadow = txt.filter(ImageFilter.GaussianBlur(radius=shadow_radius))
-        txt = shadow.point(lambda px: min(int(px * shadow_mult), 255))
+            # Calculate size of text
+            text_size = draw.textsize(the_text, font=fnt)
 
-        draw = ImageDraw.Draw(txt)
-        draw.text(position, the_text, font=fnt, fill=(255, 255, 255, int(255 * text_alpha)))
+            # Create new, smaller image to draw the text on
+            txt = Image.new(
+                'RGBA',
+                tuple(ts + pad + pos for ts, pad, pos in zip(text_size, padding, position)),
+                (0, 0, 0, 0))
 
-        # Rotate and paste onto original image
-        txt = txt.rotate(90, expand=1, resample=Image.LANCZOS)
-        alpha.paste(txt, box=tuple(a - b for a, b in zip(alpha.size, txt.size)))
-        img = Image.alpha_composite(img, alpha)
+            # Do the drawing
+            draw = ImageDraw.Draw(txt)
+            draw.text(position, the_text, font=fnt, fill=(0, 0, 0, 255))
+
+            shadow = txt.filter(ImageFilter.GaussianBlur(radius=shadow_radius))
+            txt = shadow.point(lambda px: min(int(px * shadow_mult), 255))
+
+            draw = ImageDraw.Draw(txt)
+            draw.text(position, the_text, font=fnt, fill=(255, 255, 255, int(255 * text_alpha)))
+
+            # Rotate and paste onto original image
+            txt = txt.rotate(90, expand=1, resample=Image.LANCZOS)
+            alpha.paste(txt, box=tuple(a - b for a, b in zip(alpha.size, txt.size)))
+            img = Image.alpha_composite(img, alpha)
+            image_changed = True
+
+        if not image_changed:
+            self.media = self.media_original
+            return
 
         # Save result
         bio = BytesIO()
