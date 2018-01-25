3a90a91a886a2141f616c7da5686d04b367d3b6e
Marcus Weiner <marcus.weiner@gmail.com>
2018-01-25 18:58:20 +0100

    :sparkles: Allow querying for next fragments on fragment

--- app/api/views/fragments.py
+++ app/api/views/fragments.py
@@ -1,5 +1,8 @@
+from ..utils import augment_fragments
+
 from cms.models.report import ReportFragment
 
+from distutils.util import strtobool
 from rest_framework import serializers, viewsets
 
 
@@ -9,12 +12,16 @@ class ReportFragmentSerializer(serializers.ModelSerializer):
         fields = ('id', 'question', 'text', 'media_original', 'media', 'media_note',
             'link_wiki', 'report')
 
+    def __init__(self, *args, with_next=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._with_next = with_next
+
     def to_representation(self, obj):
         output = super().to_representation(obj)
-        fragments = ReportFragment.objects.filter(report=obj.report, id__gt=obj.id)
-        fragment_out = [super(ReportFragmentSerializer, self).to_representation(fragment) for fragment in fragments]
-        output['next_fragments'] = fragment_out
+        if self._with_next:
+            output['next_fragments'] = augment_fragments(obj.report, obj.id)
         return output
+
 
 class ReportFragmentViewSet(viewsets.ModelViewSet):
     """
@@ -23,3 +30,7 @@ class ReportFragmentViewSet(viewsets.ModelViewSet):
     queryset = ReportFragment.objects.all().order_by('id')
     serializer_class = ReportFragmentSerializer
     filter_fields = ('report', )
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['with_next'] = strtobool(self.request.query_params.get('withNext', "0"))
+        return super().get_serializer(*args, **kwargs)
