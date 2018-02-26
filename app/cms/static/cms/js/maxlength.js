bcb050eb762fb37d3b4c5ab474b1aa496b9815bf
Jannes Höke <jhoeke@uni-bremen.de>
2018-02-26 13:23:06 +0100

    :bug: Recreate char counters when adding new fragments

--- app/cms/static/cms/js/maxlength.js
+++ app/cms/static/cms/js/maxlength.js
@@ -1,3 +1,12 @@
 
-$("input:text:not(.vTimeField, .vDateField)").maxlength();
-$("textarea").maxlength();
+const gen_maxlength = function () {
+    $("input:text:not(.vTimeField, .vDateField)").maxlength();
+    $("textarea").maxlength();
+};
+
+gen_maxlength();
+
+$(".add-row > a").click(function() {
+    $(".maxlength").remove();
+    gen_maxlength();
+});
