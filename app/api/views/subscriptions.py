6c9bc0ce28a5d2c36d56b3acb2debea5994daa30
ChristianJoe <cjoe@gmx.de>
2018-02-26 15:02:35 +0100

    :sparkles: Offer subscriptions API

---
+++ app/api/views/subscriptions.py
@@ -0,0 +1,14 @@
+from cms.models.subscription import Subscription
+from rest_framework import serializers, viewsets
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = ('psid', 'morning', 'evening')
+
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    filter_fields = ('psid', 'morning', 'evening')
