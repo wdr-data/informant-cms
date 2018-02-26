d11ce1c7beed4dbed9b62393091788825984a037
Jannes Höke <jhoeke@uni-bremen.de>
2018-02-26 16:46:26 +0100

    :bug: Fix django admin backend JS

--- app/cms/static/cms/js/maxlength.js
+++ app/cms/static/cms/js/maxlength.js
@@ -1,14 +1,15 @@
 
-const gen_maxlength = function () {
+const gen_maxlength = function ($) {
     $("input:text:not(.vTimeField, .vDateField)").maxlength();
     $("textarea").maxlength();
 };
 
-
-$("document").ready(function () {
-    gen_maxlength();
-    $(".add-row > a").click(function () {
-        $(".maxlength").remove();
-        gen_maxlength();
+(function ($) {
+    $("document").ready(function () {
+        gen_maxlength($);
+        $(".add-row > a").click(function () {
+            $(".maxlength").remove();
+            gen_maxlength($);
+        });
     });
-});
+})(django.jQuery);
--- app/cms/static/cms/js/unsaved_changes.js
+++ app/cms/static/cms/js/unsaved_changes.js
@@ -1,18 +1,22 @@
 
 var unsaved = false;
 
-$(":input").change(function(){ //trigers change in all input fields including text type
-    unsaved = true;
-});
-
-$('.submit-row > input').click(function() {
-    unsaved = false;
-});
-
-function unloadPage(){
-    if(unsaved){
+function unloadPage() {
+    if (unsaved) {
         return "Du hast ungespeicherte Änderungen!";
     }
 }
 
 window.onbeforeunload = unloadPage;
+
+(function ($) {
+    $("document").ready(function () {
+        $(":input").change(function () { //trigers change in all input fields including text type
+            unsaved = true;
+        });
+
+        $('.submit-row > input').click(function () {
+            unsaved = false;
+        });
+    });
+})(django.jQuery);
