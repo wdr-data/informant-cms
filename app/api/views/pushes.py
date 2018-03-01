bc86a212c3e863ca024a77a589e3b6ad41d4406f
Jannes Höke <jhoeke@uni-bremen.de>
2018-03-01 11:18:53 +0100

    :bug: Add outro media fields to API view

--- app/api/views/pushes.py
+++ app/api/views/pushes.py
@@ -2,16 +2,19 @@ from cms.models.push import Push
 from .reports import ReportSerializer
 from rest_framework import serializers, viewsets
 
+
 class PushSerializer(serializers.ModelSerializer):
-  reports = ReportSerializer(many=True, read_only=True)
-  class Meta:
-    model = Push
-    fields = ('id', 'pub_date', 'published', 'delivered',
-              'breaking', 'headline', 'intro', 'reports', 'outro'
-             )
+    reports = ReportSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Push
+        fields = ('id', 'pub_date', 'published', 'delivered',
+                  'breaking', 'headline', 'intro', 'reports', 'outro',
+                  'media', 'media_original', 'media_note',
+                  )
+
 
 class PushViewSet(viewsets.ModelViewSet):
-  queryset = Push.objects.all().order_by('-pub_date')
-  serializer_class = PushSerializer
-  filter_fields = ('published', 'delivered', 'breaking')
- 
+    queryset = Push.objects.all().order_by('-pub_date')
+    serializer_class = PushSerializer
+    filter_fields = ('published', 'delivered', 'breaking')
