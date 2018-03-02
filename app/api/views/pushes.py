3f8226bb2c2b769e421944b05c5e150ecd8c1800
Jannes Höke <jhoeke@uni-bremen.de>
2018-03-02 20:49:46 +0100

    :sparkles: Serve only published pushes and reports from the REST API

--- app/api/views/pushes.py
+++ app/api/views/pushes.py
@@ -1,10 +1,18 @@
 from cms.models.push import Push
+from rest_framework.fields import SerializerMethodField
+
 from .reports import ReportSerializer
 from rest_framework import serializers, viewsets
 
 
 class PushSerializer(serializers.ModelSerializer):
-    reports = ReportSerializer(many=True, read_only=True)
+    reports = SerializerMethodField()
+
+    def get_reports(self, obj):
+        reports_queryset = obj.reports.filter(published=True)
+        serializer = ReportSerializer(
+            instance=reports_queryset, many=True, read_only=True, context=self.context)
+        return serializer.data
 
     class Meta:
         model = Push
@@ -15,6 +23,6 @@ class PushSerializer(serializers.ModelSerializer):
 
 
 class PushViewSet(viewsets.ModelViewSet):
-    queryset = Push.objects.all().order_by('-pub_date')
+    queryset = Push.objects.filter(published=True).order_by('-pub_date')
     serializer_class = PushSerializer
     filter_fields = ('published', 'delivered', 'pub_date', 'timing')
