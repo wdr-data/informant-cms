ff204e6eae51de9c805bd9a9e9b11e04c0803376
Jannes Höke <jhoeke@uni-bremen.de>
2018-05-18 12:42:14 +0200

    :hankey: Enforce correct ordering of javascript

--- app/cms/models/__init__.py
+++ app/cms/models/__init__.py
@@ -7,3 +7,8 @@ from .faq import *
 from .wiki import *
 from .subscription import *
 from .tag import *
+
+from emoji_picker.widgets import EmojiPickerTextInput, EmojiPickerTextarea
+
+EmojiPickerTextInput.Media.js = ('admin/js/jquery.init.js', ) + EmojiPickerTextInput.Media.js
+EmojiPickerTextarea.Media.js = ('admin/js/jquery.init.js', ) + EmojiPickerTextarea.Media.js
