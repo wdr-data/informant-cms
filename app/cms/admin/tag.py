2b95c2c8c6baa9ea92435803637c0649f7a5666d
Jannes Höke <jhoeke@uni-bremen.de>
2018-06-21 15:57:42 +0200

    :hankey: Prevent user from mass-deleting tags

--- app/cms/admin/tag.py
+++ app/cms/admin/tag.py
@@ -11,6 +11,11 @@ class ReportTagAdmin(admin.ModelAdmin):
         model = ReportTag
         fields = "__all__"
 
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
 
 # Register your model here
 admin.site.register(ReportTag, ReportTagAdmin)
