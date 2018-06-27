c2dfd40d19e29729bcedc53b2756ef4f89ac5860
ChristianJoe <cjoe@gmx.de>
2018-06-27 14:07:42 +0200

    :sparkles: Make `short_headline` available in API

--- app/api/views/reports.py
+++ app/api/views/reports.py
@@ -17,8 +17,8 @@ class ReportSerializer(ModelSerializerWithFragments):
 
     class Meta:
         model = Report
-        fields = ('id', 'created', 'genres', 'topic', 'tags', 'headline', 'text', 'media',
-                  'media_original', 'media_note', 'published', 'delivered')
+        fields = ('id', 'created', 'genres', 'topic', 'tags', 'headline', 'short_headline', 'text',
+                  'media', 'media_original', 'media_note', 'published', 'delivered')
 
 
 class ReportViewSet(ModelViewSetWithFragments):
