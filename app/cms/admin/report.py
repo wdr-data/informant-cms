c6f81f05b6f664331f1dbb5e50627dde78a5bdf0
ChristianJoe <cjoe@gmx.de>
2018-09-04 12:10:18 +0200

    :lipstick: Remove author field from admin

--- app/cms/admin/report.py
+++ app/cms/admin/report.py
@@ -46,7 +46,7 @@ class ReportModelForm(NewsBaseModelForm):
     class Meta:
         model = Report
         fields = ['headline', 'short_headline', 'genres', 'tags', 'text', 'media',
-                  'media_original', 'media_note', 'created', 'published', 'delivered', 'author']
+                  'media_original', 'media_note', 'created', 'published', 'delivered']
 
 
 class ReportAdmin(NewsBaseAdmin):
