80f1e4fd1244483f13719fd895f04e3d90a4d6b7
Jannes Höke <jhoeke@uni-bremen.de>
2018-09-07 14:03:18 +0200

    :lipstick: Make quiz option text required in backend

--- app/cms/admin/quiz.py
+++ app/cms/admin/quiz.py
@@ -8,8 +8,8 @@ from .attachment import DisplayImageWidgetStackedInline
 class QuizModelForm(forms.ModelForm):
     correct_option = forms.BooleanField(required=False, label='Richtige Antwort',
         help_text='Setze hier einen Haken, wenn diese Antwort-Option die richtige ist.')
-    quiz_option= forms.CharField(
-        required=False, label='Quiz Option', widget=EmojiPickerTextInput, max_length=20,
+    quiz_option = forms.CharField(
+        required=True, label='Quiz Option', widget=EmojiPickerTextInput, max_length=20,
         help_text='Trage hier den Button-Text für eine Antwortmöglichkeit ein. '
                   'Um ein Quiz zu erstellen müssen mindestens 2, maximal 3 Buttons gegeben sein.')
     quiz_text = forms.CharField(
