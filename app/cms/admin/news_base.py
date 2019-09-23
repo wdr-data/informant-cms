8acd0c1bca6918ed1a4279652b16f7cb3d29404a
Jannes Höke <mail@jhoeke.de>
2019-09-23 11:10:28 +0200

    :children_crossing: Improve emoji picker text inputs

--- app/cms/admin/news_base.py
+++ app/cms/admin/news_base.py
@@ -1,13 +1,13 @@
 from django import forms
 from tags_input import admin as tags_input_admin
-from emoji_picker.widgets import EmojiPickerTextarea
+from emoji_picker.widgets import EmojiPickerTextareaAdmin
 
 from .attachment import AttachmentAdmin
 
 
 class NewsBaseModelForm(forms.ModelForm):
     text = forms.CharField(
-        required=True, label="Intro-Text", widget=EmojiPickerTextarea, max_length=2000)
+        required=True, label="Intro-Text", widget=EmojiPickerTextareaAdmin, max_length=2000)
 
 
 class NewsBaseAdmin(tags_input_admin.TagsInputAdmin, AttachmentAdmin):
--- app/cms/admin/quiz.py
+++ app/cms/admin/quiz.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from emoji_picker.widgets import EmojiPickerTextInput, EmojiPickerTextarea
+from emoji_picker.widgets import EmojiPickerTextInputAdmin, EmojiPickerTextareaAdmin
 
 from .attachment import DisplayImageWidgetStackedInline
 
@@ -9,11 +9,11 @@ class QuizModelForm(forms.ModelForm):
     correct_option = forms.BooleanField(required=False, label='Richtige Antwort',
         help_text='Setze hier einen Haken, wenn diese Antwort-Option die richtige ist.')
     quiz_option = forms.CharField(
-        required=True, label='Quiz Option', widget=EmojiPickerTextInput, max_length=20,
+        required=True, label='Quiz Option', widget=EmojiPickerTextInputAdmin, max_length=20,
         help_text='Trage hier den Button-Text für eine Antwortmöglichkeit ein. '
                   'Um ein Quiz zu erstellen müssen mindestens 2, maximal 3 Buttons gegeben sein.')
     quiz_text = forms.CharField(
-        required=True, label='Quiz Antwort', widget=EmojiPickerTextarea, max_length=2000,
+        required=True, label='Quiz Antwort', widget=EmojiPickerTextareaAdmin, max_length=2000,
         help_text='Hier kannst du einen individuellen Text für diese Antwortmöglichkeit eintragen')
 
 
