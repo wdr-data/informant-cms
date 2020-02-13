73e1d04c912c5668ecc3a89d5f79d08e04b87684
P3nny <p3nny@hacking.studio>
2020-02-13 15:09:20 +0100

    🚸 Add custom error page for 403 errors

--- app/cms/admin/__init__.py
+++ app/cms/admin/__init__.py
@@ -8,6 +8,6 @@ from .wiki import *
 from .tag import *
 from .profile import *
 
-admin.site.site_header = 'Informant'
-admin.site.site_title = 'Informant'
+admin.site.site_header = 'STAGING | Django WDR aktuell'
+admin.site.site_title = 'STAGING | Django WDR aktuell'
 admin.site.index_title = 'Home'
--- app/main/urls.py
+++ app/main/urls.py
@@ -36,3 +36,5 @@ urlpatterns = [
 
 if settings.MEDIA_URL and settings.MEDIA_ROOT:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler403 = cms_views.error_403
