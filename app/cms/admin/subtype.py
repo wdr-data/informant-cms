1474cc859eb454611b82e1659f43883ec8a79151
Jannes Höke <mail@jhoeke.de>
2020-03-20 09:37:03 +0100

    🚸 Add emoji selector for subtype emoji field

--- app/cms/admin/subtype.py
+++ app/cms/admin/subtype.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
+from django import forms
+from emoji_picker.widgets import EmojiPickerTextInputAdmin
 
 from ..models.subtype import Subtype
 
-class SubtypeAdmin(admin.ModelAdmin):
+class SubtypeModelForm(forms.ModelForm):
     class Meta:
         model = Subtype
         fields = "__all__"
+
+    emoji = forms.CharField(label='Emoji', widget=EmojiPickerTextInputAdmin, max_length=3)
+
+class SubtypeAdmin(admin.ModelAdmin):
+    form = SubtypeModelForm
 
 # Register your model here
 admin.site.register(Subtype, SubtypeAdmin)
